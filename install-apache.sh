73827c58eb903d81af907efcb1cb28eb1d37c128
ondrej-vaclavu <ondrej.vaclavu@outlook.com>
2019-09-30 16:14:59 +0200

    fix html

--- install-apache.sh
+++ install-apache.sh
@@ -5,7 +5,7 @@ apt-get -y update
 apt-get -y install apache2 
 
 # write some HTML
-echo \<center\>\<h1\>My Demo App\</h1\>\<br/\>\<p\>This is running on VM %0\</p\>\</center\> > /var/www/html/demo.html
+echo \<center\>\<h1\>My Demo App\</h1\>\<br/\>\<p\>This is running on VM $1\</p\>\</center\> > /var/www/html/demo.html
 
 # restart Apache
 apachectl restart
